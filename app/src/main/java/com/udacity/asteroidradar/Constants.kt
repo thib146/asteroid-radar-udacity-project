7c31843eedc7bcc573084893dc9c78c3039f7721
thib146 <thibaut@jumboprivacy.com>
2023-08-12 16:12:24 +0200

    remove NASA API key

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "gVf2I491bW92iFzVlnh22ae81gTsWzQw3QAYlksx"
+    const val API_KEY = "INSERT API KEY HERE"
 }
